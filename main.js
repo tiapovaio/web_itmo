c7812ff0a1929e164c21fda5ed136e33c5464aeb
tiapovaio <tiapovaiooo@icloud.com>
2024-11-21 16:04:18 +0300

    lab5 fix

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-    const reviews = JSON.parse(localStorage.getItem('reviews')) || [];
-
+window.addEventListener('DOMContentLoaded', function() {
+    const reviews = JSON.parse(localStorage.getItem('rev')) || [];
     const reviewsList = document.getElementById('reviews-list');
+    const template = document.getElementById('review-card-template');
 
-    reviews.forEach((review) => {
-    const reviewElement = document.createElement('div');
-    reviewElement.className = 'review';
-    reviewElement.innerHTML = `
-        <h4>${review.name}</h4>
-        <p>${review.feedback}</p>
-      `;
-    reviewsList.appendChild(reviewElement);
+    reviews.forEach(function(review) {
+        const reviewCard = template.content.cloneNode(true);
+
+        reviewCard.querySelector('.review-name').textContent = review.name;
+        reviewCard.querySelector('.review-feedback').textContent = review.feedback;
+        reviewCard.querySelector('.review-date').textContent = review.date;
+
+        reviewsList.appendChild(reviewCard);
+    });
 });
+
+
+//     почему оно работает
+// способы хранения крроме локал
--- script.js
+++ script.js
@@ -2,19 +2,21 @@ document.getElementById('submit-btn').addEventListener('click', function () {
     const name = document.getElementById('name').value.trim();
     const feedback = document.getElementById('feedback').value.trim();
 
-    if (!name || !feedback) {
-        alert('Пожалуйста, заполните все поля формы!');
-        return;
+    if (name === '' || feedback === '') {
+        alert('Пожалуйста, заполните все поля!');
+    } else {
+        document.getElementById('thank-you-message').style.display = 'block';
     }
+
     const review = {
-        name,
-        feedback,
-        date: new Date().toLocaleString(),
+        name: name,
+        feedback: feedback,
+        date: new Date().toLocaleDateString()
     };
 
-    let reviews = JSON.parse(localStorage.getItem('reviews')) || [];
+    let reviews = JSON.parse(localStorage.getItem('rev')) || [];
     reviews.push(review);
-    localStorage.setItem('reviews', JSON.stringify(reviews));
+    localStorage.setItem('rev', JSON.stringify(reviews));
 
-    window.location.href = 'main.html';
+    document.getElementById('feedback-form').reset();
 });
